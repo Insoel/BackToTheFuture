c05f2ddbe8113c18b04d22590fcd85e7d1a58f80
Insoel <insoel@gmail.com>
2021-03-19 11:01:38 +0000

    Remove HDRP, Add URP

---
+++ Unity/BackToTheFuture/Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour
+{
+	private void Update()
+	{
+		Move(transform.position + new Vector3(0.1f, 0f));
+	}
+
+	protected virtual void Move(Vector2 newPos)
+	{
+		transform.position = newPos;
+	}
+}
