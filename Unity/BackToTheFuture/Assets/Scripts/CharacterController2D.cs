e106563075ebbedc4d930815c96221016ce2400a
Insoel <insoel@gmail.com>
2021-04-17 16:02:26 +0100

    Add jump functionality, Improve movement

--- Unity/BackToTheFuture/Assets/Scripts/CharacterController2D.cs
+++ Unity/BackToTheFuture/Assets/Scripts/CharacterController2D.cs
@@ -7,13 +7,50 @@ public abstract class CharacterController2D : MonoBehaviour
 {
 	protected Rigidbody2D rb;
 
+
+	[Header("Movement Values")]
 	[SerializeField] protected float moveSpeed = 2f;
-	[SerializeField] protected float jumpForce = 400f;
+	[SerializeField] protected float jumpForce = 10f;
+
+	[Header("Collider Settings")]
+	[SerializeField] protected Transform groundChecker;
+	[Tooltip("This is the layer the ground uses.")]
+	[SerializeField] protected LayerMask groundLayer;
+
+	protected bool isGrounded = true;
+
+	const float GROUNDED_RADIUS = 0.1f;
 
 	protected virtual void Awake()
 	{
 		rb = GetComponent<Rigidbody2D>();
 	}
 
-	protected abstract void Move(Vector2 newPos);
+	protected virtual void FixedUpdate()
+	{
+		UpdateGroundCollision();
+	}
+
+	protected virtual void Move(Vector2 movement)
+	{
+		rb.velocity = movement;
+	}
+
+	private void UpdateGroundCollision()
+	{
+		isGrounded = false;
+
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundChecker.position, GROUNDED_RADIUS, groundLayer);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+				isGrounded = true;
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawSphere(groundChecker.position, GROUNDED_RADIUS);
+	}
 }
--- Unity/BackToTheFuture/Assets/Scripts/PlayerController2D.cs
+++ Unity/BackToTheFuture/Assets/Scripts/PlayerController2D.cs
@@ -11,20 +11,22 @@ public class PlayerController2D : CharacterController2D
 		UpdateMovement();
 	}
 
-	// Gets the user input and moves the player along that axis.
+	// Gets the user input and moves/jumps the player along that axis.
 	private void UpdateMovement()
 	{
 		Vector2 movement = new Vector2();
-		movement.x = Input.GetAxis("Horizontal") * moveSpeed;
+		movement.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
 		movement.y = rb.velocity.y;
-		if (Input.GetAxis("Vertical") > 0f) rb.AddForce(new Vector2(0f, jumpForce));
 
-		if (movement != Vector2.zero) Move(movement);
-	}
-
-	protected override void Move(Vector2 movement)
-	{
-		Debug.Log("Moved");
-		rb.velocity = movement;
+		if (movement.x != 0f)
+		{
+			Move(movement);
+		}
+		
+		if (Input.GetAxis("Vertical") > 0f && isGrounded)
+		{
+			isGrounded = false;
+			rb.AddForce(new Vector2(0f, jumpForce));
+		}
 	}
 }
