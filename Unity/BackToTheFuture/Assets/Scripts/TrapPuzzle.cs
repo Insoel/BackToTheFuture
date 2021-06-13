2ff1c8e349b3718037bc6739a73f4fc5880e9496
Insoel <insoel@gmail.com>
2021-06-13 20:42:56 +0100

    Add level01

---
+++ Unity/BackToTheFuture/Assets/Scripts/TrapPuzzle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPuzzle : Puzzle
+{
+	[SerializeField] private GameObject ropeTrap = default;
+
+	protected override void OnPuzzleCompleted()
+	{
+		ropeTrap.SetActive(true);
+	}
+}
