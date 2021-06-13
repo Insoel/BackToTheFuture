4d472fd91a2e737f5ea4276664483ad460c69f5f
Insoel <insoel@gmail.com>
2021-06-13 21:45:30 +0100

    Add gear piece

--- Unity/BackToTheFuture/Assets/Scripts/ActivateTrapPuzzle.cs
+++ Unity/BackToTheFuture/Assets/Scripts/ActivateTrapPuzzle.cs
@@ -6,11 +6,13 @@ public class ActivateTrapPuzzle : Puzzle
 {
     [SerializeField] private HingeJoint2D ropeJoint = default;
     [SerializeField] private DistanceJoint2D anchorPoint = default;
+	[SerializeField] private GameObject gearPiece = default;
 
 	protected override void OnPuzzleCompleted()
 	{
 		anchorPoint.connectedBody = null;
 		ropeJoint.connectedBody = null;
-		base.OnPuzzleCompleted();
+		LeanTween.rotateAroundLocal(gearPiece, Vector3.forward, 360f, 5f).setLoopClamp();
+		LeanTween.move(gearPiece, gearPiece.transform.position + new Vector3(3f, 0f, 0f), 1f);
 	}
 }
